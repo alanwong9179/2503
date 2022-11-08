95f6556f9b9aa03bd462028b6c71d3d8d09b7587
alanwong9179 <alanwong9179@gmail.com>
2022-11-08 16:12:17 +0800

    #

--- App.js
+++ App.js
@@ -13,9 +13,15 @@ import { manipulateAsync, FlipType, SaveFormat } from 'expo-image-manipulator';
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
-  const [blur, setBlur] = useState(0)
+  
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const [filterSetting, setFilterSetting] = [{
+    hue: 0,
+    blur: 0,
+    
+  }]
 
   useEffect(() => {
     console.log(selectedImage)
@@ -31,44 +37,44 @@ export default function App() {
     const MAXGL = 2048
     let isOverSized = false;
     // size checking, cannot > MAXGL
-    if (pickerResult.height > pickerResult.width){
+    if (pickerResult.height > pickerResult.width) {
       //h 
-      if (pickerResult.height > MAXGL){
+      if (pickerResult.height > MAXGL) {
         updatedH = MAXGL
         updatedW = MAXGL / (pickerResult.height / pickerResult.width)
         isOverSized = true
       }
-    }else if (pickerResult.width < pickerResult.height){
+    } else if (pickerResult.width < pickerResult.height) {
       //v
-      if (pickerResult.width > MAXGL){
+      if (pickerResult.width > MAXGL) {
         updatedW = MAXGL
         updatedH = MAXGL / (pickerResult.width / pickerResult.height)
         isOverSized = true
       }
-    }else{
+    } else {
       //squra
-      if (pickerResult.width > MAXGL){
+      if (pickerResult.width > MAXGL) {
         updatedH = MAXGL
         updatedW = MAXGL
         isOverSized = true
-        }
+      }
     }
 
-    if(isOverSized){
+    if (isOverSized) {
       const rotateImg = await manipulateAsync(
-        pickerResult.uri, 
-        [{rotate: 360}, {resize:{height: updatedH, width: updatedW}}], 
-        {compress: 1, format: SaveFormat.JPEG}
+        pickerResult.uri,
+        [{ rotate: 360 }, { resize: { height: updatedH, width: updatedW } }],
+        { compress: 1, format: SaveFormat.JPEG }
       )
       setSelectedImage({
         imgUri: rotateImg.uri,
         imgHeight: rotateImg.height,
         imgWidth: rotateImg.width
       })
-    }else{
-      setSelectedImage({ 
-        imgUri: pickerResult.uri, 
-        imgHeight: pickerResult.height, 
+    } else {
+      setSelectedImage({
+        imgUri: pickerResult.uri,
+        imgHeight: pickerResult.height,
         imgWidth: pickerResult.width,
       })
     }
@@ -84,28 +90,53 @@ export default function App() {
     })
   }
 
-  const openCam = () => {
-
-    return (
-      <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-    )
+  const INITZOOM = (value) => {
+    switch(value){
+      case value > 0 && value < 1000 : 
+        return 0.4
+      case value > 1001 && value < 2000 :
+        return 0.3
+      case value > 2000 :
+        return 0.2
+      default: 
+       return 0.2
+    }
   }
 
   return (
     <SafeAreaView style={styles.container}>
+      <View style={{ borderWidth: 1, flex: 1 }}>     
+      {
+          selectedImage !== null &&
+          <ReactNativeZoomableView
+            maxZoom={1.5}
+            minZoom={0.1}
+            zoomStep={0.5}
+            initialZoom={INITZOOM(selectedImage.imgHeight)}
+           
+          >
+            <Surface
+              style={{
+                height: selectedImage.imgHeight,
+                width: selectedImage.imgWidth
+              }}
+            //  ref={ref => (this.image = ref)}
+            >
+              <ImageFilters
+                height={selectedImage.imgHeight}
+                width={selectedImage.imgWidth}
+                blur={blur}>
+                {{ uri: selectedImage.imgUri }}
+              </ImageFilters>
+            </Surface>
+
+          </ReactNativeZoomableView>
+          }
+
+      </View>
+
       <View style={{ flex: 1, backgroundColor: '#0022ff' }}>
-      <TouchableOpacity onPress={openCam} style={{ alignItems: 'center' }}>
-          <Text>Camera</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={openImagePickerAsync} style={{ alignItems: 'center' }}>
+        <TouchableOpacity onPress={openImagePickerAsync} style={{ alignItems: 'center' }} >
           <Text style={styles.buttonText}>Pick a photo</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={saveImage} style={{ alignItems: 'center' }}>
@@ -115,32 +146,7 @@ export default function App() {
           <Text>blur</Text>
         </TouchableOpacity>
       </View>
-      <View style={{borderWidth: 1, flexShrink: 1, height: 500, width: 300 }}>
-      <ReactNativeZoomableView
-          maxZoom={1.5}
-          minZoom={0.1}
-          zoomStep={0.5}
-        >
-         {
-          selectedImage !== null &&
- 
-          <Surface 
-          style={{ 
-            height: selectedImage.imgHeight, 
-            width: selectedImage.imgWidth
-            }} 
-            ref={ref => (this.image = ref)}>
-          <ImageFilters 
-              height={selectedImage.imgHeight }
-              width={selectedImage.imgWidth }
-              blur={blur} >
-                  {{ uri: selectedImage.imgUri }}
-            </ImageFilters>
-          </Surface>
-}
-        
-        </ReactNativeZoomableView>
-        </View>
+
 
     </SafeAreaView>
   );
